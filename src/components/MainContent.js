e3bddb43bd0ef1d5ef3548660a3644baf8868390
aleksandar angelevski <stotka123@yahoo.com>
2022-05-09 13:27:28 +0200

    start-projects

---
+++ src/components/MainContent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MainContent.css'
+
+
+const MainContent = () => {
+    return (
+        <div className='container'>main</div>
+    )
+}
+
+
+export default MainContent
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+ 
+
+
+
+const Navbar = () => {
+    return (
+        <div>navbar</div>
+    )
+}
+
+export default Navbar
+
+
+
+
