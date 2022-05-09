00c1ecdda92c26f0ec27d2a0d9deb67e960776f2
aleksandar angelevski <stotka123@yahoo.com>
2022-05-09 13:45:03 +0200

    set-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from "react";
-import MainContent from "./components/MainContent";
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Create from "./pages/create/Create";
+import Home from "./pages/home/Home";
+import Recipe from "./pages/recipe/Recipe";
+import Search from "./pages/search/Search";
 
 function App() {
   return (
-    <div><Navbar/>
-         <MainContent/>
-    
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/recipe:id">
+            <Recipe />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
-    
   );
 }
 
---
+++ src/pages/recipe/Recipe.js
@@ -0,0 +1,9 @@
+import "./Recipe.css";
+
+import React from "react";
+
+function Recipe() {
+  return <div>Recipe</div>;
+}
+
+export default Recipe;
